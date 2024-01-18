a18f879d7d5696a125aeab13b2ed273ac89c0e05
mirzaarslan <mirzaarslan450@gmail.com>
2024-01-18 15:25:00 +0400

    dependencies added

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
     id("com.android.library").version("8.0.2").apply(false)
     kotlin("android").version("1.8.21").apply(false)
     kotlin("multiplatform").version("1.8.21").apply(false)
+    id("com.google.devtools.ksp").version("1.9.21-1.0.16").apply(false)
+    id("com.rickclephas.kmp.nativecoroutines").version("1.0.0-ALPHA-23").apply(false)
 }
 
 tasks.register("clean", Delete::class) {
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     kotlin("multiplatform")
     id("com.android.library")
+    kotlin("plugin.serialization") version "1.8.21"
+    id("com.google.devtools.ksp")
+    id("com.rickclephas.kmp.nativecoroutines")
 }
 
 @OptIn(org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi::class)
@@ -24,11 +27,33 @@ kotlin {
             baseName = "shared"
         }
     }
-
+    val ktorVersion = "2.3.5"
+    val coroutinesVersion = "1.7.3"
     sourceSets {
         val commonMain by getting {
             dependencies {
-                //put your multiplatform dependencies here
+                implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+                //ktor for networking
+                implementation("io.ktor:ktor-client-core:$ktorVersion")
+                implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+                implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+                implementation("io.ktor:ktor-client-logging:$ktorVersion")
+                //koin for dependency injection
+                implementation("io.insert-koin:koin-core:3.5.3")
+                implementation("io.insert-koin:koin-compose:1.1.2")
+                //napier for logging
+                implementation("io.github.aakira:napier:2.6.1")
+            }
+        }
+        val androidMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-android:$ktorVersion")
+            }
+        }
+        val iosMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-darwin:$ktorVersion")
             }
         }
         val commonTest by getting {
@@ -38,7 +63,9 @@ kotlin {
         }
     }
 }
-
+kotlin.sourceSets.all {
+    languageSettings.optIn("kotlin.experimental.ExperimentalObjCName")
+}
 android {
     namespace = "com.arslan.starprofileskmm"
     compileSdk = 33
